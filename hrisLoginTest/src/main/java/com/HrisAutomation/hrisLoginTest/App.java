66b3ececc33bcaacdc4d2d53ef4acac9cbc1f95d
KRISHNA KUAMR <krk5512@gmail.com>
2018-07-03 14:43:19 +0530

    Update App.java

--- hrisLoginTest/src/main/java/com/HrisAutomation/hrisLoginTest/App.java
+++ hrisLoginTest/src/main/java/com/HrisAutomation/hrisLoginTest/App.java
@@ -5,68 +5,62 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
 
 public class App 
 {
-   WebDriver driver;	
-	@FindBy (id = "txtUserName")
-	private WebElement username;
-	
-	@FindBy (id = "txtPassword")
-	private WebElement password;
-	
-	@FindBy (name = "Submit")
-	private WebElement submitButton;
-	
+	static WebDriver driver;	
+
 	public WebElement GetUserNameEntry(){
-		   return this.username;
-	   }
-	   
-	   public WebElement GetPasswordEntry()
-	   {
-		   return this.password;
-	   }
-	
-	
-   public void fillCredentials(String username,String password)
-   {
-	   System.out.println(username);
-	   System.out.println(password);
+		return this.driver.findElement(By.id("txtUserName"));
+	}
 
-	   GetUserNameEntry().sendKeys(username);
-	   GetPasswordEntry().sendKeys(password);
-	   driver.findElement(By.linkText("Sign In")).click();
-   }
+	public WebElement GetPasswordEntry()
+	{
+		return this.driver.findElement(By.id("txtPassword"));
+	}
 
-public String WrongPassword(String username, String password) {
-            fillCredentials(username,password);
-            return getErrorMessageElement().getText();
-}
+	public void launchHris() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\prkrishnakumar\\eclipse-workspace\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://hris.qainfotech.com/login.php");
+	}
 
-private WebElement getErrorMessageElement() {
-    return this.driver.findElement(By.cssSelector(".alert-error"));
-}
+	public void fillCredentials(String username,String password)
+	{
+		System.out.println(username);
+		System.out.println(password);
+		GetUserNameEntry().clear();
+		GetPasswordEntry().clear();
+		GetUserNameEntry().sendKeys(username);
+		GetPasswordEntry().sendKeys(password);
+		driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[2]/div/input")).click();
+	}
+	public String WrongPassword(String username, String password) {
+		fillCredentials(username,password);
+		return getErrorMessageElement().getText();
+	}
+	private WebElement getErrorMessageElement() {
+		return this.driver.findElement(By.cssSelector(".alert-error"));
+	}
 
-public void launchHris() throws InterruptedException {
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\prkrishnakumar\\eclipse-workspace\\chromedriver.exe");
-	  driver = new ChromeDriver();
-	  driver.get("https://hris.qainfotech.com/login.php");
-	  Thread.sleep(2000);
-}
+	public String NoPassword(String username ,String password) {
+		fillCredentials(username,password);
+		return driver.findElement(By.id("txtPassword")).getAttribute("class");
+	}
+	public TestTimesheet CorrectCredentials(String username, String password) {
+		fillCredentials(username,password);
+		return new TestTimesheet(driver);
+	}
+	public Boolean isPasswordEntryAnnotated(){
+		return GetPasswordEntry().getAttribute("style").contains("red");
+	}
+	public TestTimesheet logoutUserAccount()
+	{
+		return new TestTimesheet(driver);
+	}
+	public void closeBrowser() {
+		driver.quit();
+	}
 
-
-public String NoPassword(String username ,String password) {
-    fillCredentials(username,password);
-	return driver.findElement(By.id("txtPassword")).getAttribute("class");
-
-}
-
-public TestTimesheet CorrectCredentials(String username, String password) {
-    fillCredentials(username,password);
-	return new TestTimesheet(driver);
-}
-public Boolean isPasswordEntryAnnotated(){
-    return GetPasswordEntry().getAttribute("style").contains("red");
-}
-   
 }
